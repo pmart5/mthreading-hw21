7d9d14fdd5e6c48fc67e0657e9ddc081cba41f98
Пётр Мартынчик <pmart5a@yandex.ru>
2022-05-20 00:46:04 +0400

    Изменена структура классов Main, Car, CarDealership, CarFactory.

--- src/main/java/com/pmart5a/myclasses/CarDealership.java
+++ src/main/java/com/pmart5a/myclasses/CarDealership.java
@@ -4,15 +4,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CarDealership {
-    CarFactory carFactory = new CarFactory(this);
-    static List<Car> cars = new ArrayList<>(CarFactory.CAR_RELEASE_PLAN);
 
-    public List<Car> sellCar() {
-        return carFactory.sellCars();
+    public static final int TIME_TO_SHOP = 1000;
+    public static final int CAR_SUPPLY_PLAN = 11;
+    private static final List<Car> cars = new ArrayList<>(CAR_SUPPLY_PLAN);
+    private final CarFactory carFactory = new CarFactory();
+    private boolean stopSales = false;
+
+    public synchronized void acceptCar() {
+        try {
+            for (int i = 0; i < CAR_SUPPLY_PLAN; i++) {
+                System.out.printf("%s: собираю автомобиль.\n", Thread.currentThread().getName());
+                getCars().add(carFactory.assembleTheCar());
+                System.out.printf("%s: выпущен 1 автомобиль.\n", Thread.currentThread().getName());
+                if (i == CAR_SUPPLY_PLAN - 1) {
+                    stopSales = true;
+                    notifyAll();
+                    break;
+                } else {
+                    notify();
+                    wait();
+                }
+            }
+            Thread.currentThread().interrupt();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void acceptCar() {
-        carFactory.receiveCar();
+    public synchronized List<Car> sellCar() {
+        List<Car> purchasedCars = new ArrayList<>();
+        while (!stopSales) {
+            try {
+                System.out.printf("%s зашёл в автосалон.\n", Thread.currentThread().getName());
+                if (getCars().isEmpty()) {
+                    System.out.println("Машин нет!");
+                } else {
+                    Thread.sleep(TIME_TO_SHOP);
+                    Car car = getCars().remove(0);
+                    System.out.printf("%s уехал на новом авто марки '%s' модель '%s %d'.\n",
+                            Thread.currentThread().getName(), car.getCarBrand(), car.getCarModel(),
+                            car.getEnginePower());
+                    purchasedCars.add(car);
+                    notifyAll();
+                }
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.printf("%s, всего куплено автомобилей: %d.\n", Thread.currentThread().getName(), purchasedCars.size());
+        Thread.currentThread().interrupt();
+        return purchasedCars;
     }
 
     static List<Car> getCars() {
--- src/main/java/com/pmart5a/myclasses/CarFactory.java
+++ src/main/java/com/pmart5a/myclasses/CarFactory.java
@@ -1,65 +1,27 @@
 package com.pmart5a.myclasses;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 public class CarFactory {
 
-    public static final int CAR_RELEASE_PLAN = 11;
     public static final int TIME_ASSEMBLY_CAR = 3000;
-    public static final int TIME_TO_SHOP = 1000;
-    private boolean stopSales = false;
-    private final CarDealership carDealership;
+    private static final String carBrand = "ЖМИ";
+    private static final String[] carModels = {"Тарантас", "Вжик", "Вездеход"};
+    private static final int[] enginePowers = {90, 370, 700};
 
-    public CarFactory(CarDealership carDealership) {
-        this.carDealership = carDealership;
-    }
-
-    public synchronized void receiveCar() {
+    public Car assembleTheCar() {
+        int modelNumber = getCarModelRandom();
+        String carModel = carModels[modelNumber];
+        int enginePower = enginePowers[modelNumber];
         try {
-            for (int i = 0; i < CAR_RELEASE_PLAN; i++) {
-                System.out.printf("%s: собираю автомобиль.\n", Thread.currentThread().getName());
-                Thread.sleep(TIME_ASSEMBLY_CAR);
-                CarDealership.getCars().add(new Car());
-                System.out.printf("%s: выпущен 1 автомобиль.\n", Thread.currentThread().getName());
-                if (i == CAR_RELEASE_PLAN - 1) {
-                    stopSales = true;
-                    notifyAll();
-                    break;
-                } else {
-                    notify();
-                    wait();
-                }
-            }
-            Thread.currentThread().interrupt();
+            Thread.sleep(CarFactory.TIME_ASSEMBLY_CAR);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        return new Car(carBrand, carModel, enginePower);
     }
 
-    public synchronized List<Car> sellCars() {
-        List<Car> purchasedCars = new ArrayList<>(CAR_RELEASE_PLAN);
-        while (!stopSales) {
-            try {
-                System.out.printf("%s зашёл в автосалон.\n", Thread.currentThread().getName());
-                if (CarDealership.getCars().size() == 0) {
-                    System.out.println("Машин нет!");
-                } else {
-                    Thread.sleep(TIME_TO_SHOP);
-                    Car car = CarDealership.getCars().remove(0);
-                    System.out.printf("%s уехал на новом авто марки '%s' модель '%s %d'.\n",
-                            Thread.currentThread().getName(), car.getCarBrand(), car.getCarModel(),
-                            car.getEnginePower());
-                    purchasedCars.add(car);
-                    notifyAll();
-                }
-                wait();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        System.out.printf("%s, всего куплено автомобилей: %d.\n", Thread.currentThread().getName(), purchasedCars.size());
-        Thread.currentThread().interrupt();
-        return purchasedCars;
+    private int getCarModelRandom() {
+        return new Random().nextInt(carModels.length);
     }
 }
